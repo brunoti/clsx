06b1189fe04073ab4bef80579ff9e6ded4ed2e91
Bruno Oliveira <brunohdbass@gmail.com>
2024-06-10 12:47:47 -0300

    fix: use valueOf to check for thruthyness

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
-const is = (x, t) => Object.prototype.toString.call(x) === `[object ${t}]`;
+const is = (val, type) => Object.prototype.toString.call(val) === `[object ${type}]`;
 
 function toVal(mix) {
 	var k, y, str='';
 
 	if (is(mix, 'String') || is(mix, 'Number')) {
 		str += mix;
-	} else if (is(mix, 'Object')) {
-		if (Array.isArray(mix)) {
+	} else if (is(mix, 'Array')) {
 			var len=mix.length;
 			for (k=0; k < len; k++) {
-				if (mix[k]) {
+				if (mix[k] && mix[k].valueOf()) {
 					if (y = toVal(mix[k])) {
 						str && (str += ' ');
 						str += y;
 					}
 				}
 			}
-		} else {
-			for (y in mix) {
-				if (mix[y]) {
-					str && (str += ' ');
-					str += y;
-				}
+	} else if (is(mix, 'Object')) {
+		for (y in mix) {
+			if (mix[y] && mix[y].valueOf()) {
+				str && (str += ' ');
+				str += y;
 			}
 		}
 	}
@@ -32,7 +30,7 @@ function toVal(mix) {
 export function clsx() {
 	var i=0, tmp, x, str='', len=arguments.length;
 	for (; i < len; i++) {
-		if (tmp = arguments[i]) {
+		if ((tmp = arguments[i]) && tmp.valueOf()) {
 			if (x = toVal(tmp)) {
 				str && (str += ' ');
 				str += x
